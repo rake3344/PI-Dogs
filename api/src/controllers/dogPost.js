f2187a4f862d2ca907d9e542b81693371a760c59
rake3344 <juan.agudelo17@udea.edu.co>
2023-03-01 15:04:26 -0500

    rutas terminadas

--- api/src/controllers/dogPost.js
+++ api/src/controllers/dogPost.js
@@ -36,43 +36,6 @@ const dogPost = async (req, res) => {
         res.status(400).json({error: error.message})
     }
 
-
-
-
-
-
-    // const { Imagen, Nombre, Altura, Peso, Años_de_vida, Temperamento } = req.body;
-    // try {
-    //     const dog = await Dog.findOrCreate({
-    //         where: {
-    //             Nombre: Nombre,
-    //             Imagen: Imagen,
-    //             Altura: Altura,
-    //             Peso: Peso,
-    //             Años_de_vida: Años_de_vida
-    //         }
-    //     });
-    //     if(Temperamento){
-    //         const te = Temperamento.split(',')
-    //         te.map(async t => {
-    //             try {
-    //                 const temp = await Temperament.findOrCreate({
-    //                     where: {
-    //                         Nombre: t
-    //                     }
-    //                 });
-    //                 dog.addTemperament(temp[0])
-    //             } catch (error) {
-    //                 res.status(400).json({error: error.message})
-    //             }
-    //         })
-    //     }
-    //     res.status(200).json(dog)
-    // } catch(error){
-    //     res.status(400).json({error: error.message})
-    // }
-
-
 }
 
 module.exports = dogPost;
